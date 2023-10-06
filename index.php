7db05172128eead62edfdce0f6c4ca08c2e37eda
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-10-06 13:23:50 +0300

    Make a PHP Router [02-15] (part 02)

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
 <?php
 
 require "functions.php";
-
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-if ($uri === '/') {
-    require('controllers/index.php');
-} elseif ($uri === '/about') {
-   require('controllers/about.php');
-} elseif ($uri === '/contact') {
-   require('controllers/contact.php');
-}
+require "router.php";

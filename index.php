8cf432cbefe9e88b2dd75f6d1a61cd0587068e98
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-10-07 16:27:02 +0300

    Environments and Configuration Flexibility [02-19]

--- index.php
+++ index.php
@@ -4,7 +4,10 @@ require "functions.php";
 // require "router.php";
 require "Database.php";
 
-$db = new Database();
+$config = require("config.php");
+
+$db = new Database($config['database'], 'dev', '66546654');
+
 $posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
 
 foreach ($posts as $post) {

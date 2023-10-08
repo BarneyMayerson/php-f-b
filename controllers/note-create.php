a651bfed8aa6032f69d290313b80ca317e7e52db
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-10-08 15:23:32 +0300

    Always Escape Untrusted Input [03-26] (vi)

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,7 +6,10 @@ $db = new Database($config['database'], 'dev', '66546654');
 $heading = "Create Note";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    dd($_POST);
+    $db->query("INSERT INTO notes(body, user_id) VALUES(:body, :user_id)", [
+        'body' => $_POST["body"],
+        'user_id' => 1,
+    ]);
 }
 
 require("views/note-create.view.php");

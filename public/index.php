5a0a2b575bb3e4d126b63fadfe591e5e969315ea
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-10-10 11:12:02 +0300

    Namespacing: What, Why, How? [04-31] before get started

--- public/index.php
+++ public/index.php
@@ -6,10 +6,10 @@ error_reporting(E_ALL);
 
 const BASE_PATH = __DIR__ . '/../';
 
-require BASE_PATH . "functions.php";
+require BASE_PATH . "Core/functions.php";
 
 spl_autoload_register(function($class) {
     require base_path("Core/{$class}.php");
 });
 
-require base_path("router.php");
+require base_path("Core/router.php");

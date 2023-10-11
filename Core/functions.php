7a522f03f2cd31ec6a7ce0323af4c413f0cffbe1
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-10-11 16:44:48 +0300

    Build a Better Router [04-33] part 01 (vi)

--- Core/functions.php
+++ Core/functions.php
@@ -16,17 +16,29 @@ function urlIs($value)
     return $_SERVER['REQUEST_URI'] === $value;
 }
 
-function authorize($condition, $status = Response::FORBIDDEN) {
+function abort($code = 404)
+{
+    http_response_code($code);
+
+    require base_path("views/{$code}.php");
+
+    die();
+}
+
+function authorize($condition, $status = Response::FORBIDDEN) 
+{
     if (!$condition) {
         abort($status);
     }
 }
 
-function base_path($path) {
+function base_path($path) 
+{
     return BASE_PATH . $path;
 }
 
-function view($path, $attributes = []) {
+function view($path, $attributes = []) 
+{
     extract($attributes);
     
     require base_path('views/' . $path);

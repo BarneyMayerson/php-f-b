a08e3b5c2e7423f4ef4a4e393aae0af67623ec05
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-10-12 14:25:17 +0300

    One Request, One Controller [04-34]

---
+++ controllers/notes/destroy.php
@@ -0,0 +1,20 @@
+<?php
+
+use Core\Database;
+
+$config = require base_path("config.php");
+$db = new Database($config['database'], 'dev', '66546654');
+
+$currentUserId = 1;
+
+$note = $db->query("select * from notes where id = :id", ['id' => $_POST['id']])->findOrFail();
+
+authorize($note['user_id'] === $currentUserId);
+
+$db->query('delete from notes where id = :id', [
+    'id' => $_POST['id'],
+]);
+
+header('location: /notes');
+
+exit();

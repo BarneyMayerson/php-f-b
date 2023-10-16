a544277da0b1c7ff4853c0239ef2f39dd0cf85bd
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-10-16 15:23:40 +0300

    Log In and Log Out [05-41] part 02 logout

--- controllers/session/store.php
+++ controllers/session/store.php
@@ -18,7 +18,7 @@ if (!Validator::string($password)) {
 }
 
 if (! empty($errors)) {
-    return view('/sessions/create.view.php', [
+    return view('/session/create.view.php', [
         'errors' => $errors,
     ]);
 }
@@ -38,7 +38,7 @@ if ($user) {
     }      
 }
 
-return view('/sessions/create.view.php', [
+return view('/session/create.view.php', [
     'errors' => [
           'email' => 'No matching account found for that email address and password.',
     ],
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -10,7 +10,9 @@
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
               <a href="/" class="<?= urlIs('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium">Home</a>
               <a href="/about" class="<?= urlIs('/about') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium">About</a>
-              <a href="/notes" class="<?= urlIs('/notes') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium">Notes</a>
+              <?php if ($_SESSION['user'] ?? false) : ?>
+                <a href="/notes" class="<?= urlIs('/notes') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium">Notes</a>
+              <?php endif; ?>
               <a href="/contact" class="<?= urlIs('/contact') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium">Contact</a>
             </div>
           </div>
@@ -40,6 +42,17 @@
                 <?php endif; ?>
               </div>
             </div>
+
+            <?php if($_SESSION['user'] ?? false) : ?>
+              <div class="ml-3">
+                <form action="/session" method="POST">
+                  <input type="hidden" name="_method" value="DELETE" />
+
+                  <button type="submit" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Log Out</button>
+                </form>
+              </div>
+            <?php endif; ?>
+          
           </div>
         </div>
         <div class="-mr-2 flex md:hidden">
